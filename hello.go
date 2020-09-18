9921df73360e31e88abfa65fbcc6f25e8bd4f8f1
Han Jang Hyun <wkdgus116490@gmail.com>
2020-09-18 23:34:51 +0900

    Initial commit

---
+++ hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	fmt.Println('C')
+	fmt.Println("\n")
+	fmt.Println(true)
+	fmt.Println(5 != 5)
+	fmt.Println("Hello, \"playground\"")
+	fmt.Println(math.Ceil(3.14))
+	fmt.Println(strings.Title("Hello go~"))
+}
---
+++ hi.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var id int
+	var x, y float64
+	var human bool = true
+
+	id = 55
+	x = 0
+	y = 0
+	name := "trevor"
+
+	fmt.Println(id)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(name)
+	fmt.Println(human)
+
+	//fmt.Println(math.Ceil(24.4))
+	//fmt.Println(reflect.TypeOf('B'))
+}
